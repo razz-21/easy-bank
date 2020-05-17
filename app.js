c988452bb2997f2a0f945fb167623b5f922656a3
Ernesto Razo <ernesto@hyperstacksinc.com>
2020-05-17 18:22:44 +0800

    toggle sidebar icons.

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const overlay = document.getElementById('overlay');
 
 function toogleNavbar () {
     if (!navbarMobile.classList.contains('show')) {
+        navbarToogleBtn.querySelector('img').setAttribute('src', './images/icon-close.svg');
         navbarMobile.classList.remove('display-none');
         overlay.classList.remove('display-none');
         navbarMobile.classList.add('show');
         overlay.classList.add('show');
         body.style.overflow = "hidden";
     } else {
+        navbarToogleBtn.querySelector('img').setAttribute('src', './images/icon-hamburger.svg');
         navbarMobile.classList.remove('show');
         overlay.classList.remove('show');
         body.removeAttribute('style');
