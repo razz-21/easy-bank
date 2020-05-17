73e8080c11734d0d052d1b349d021f8098307941
Ernesto Razo <ernesto@hyperstacksinc.com>
2020-05-17 18:16:39 +0800

    add button styles.

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const overlay = document.getElementById('overlay');
 function toogleNavbar () {
     if (!navbarMobile.classList.contains('show')) {
         navbarMobile.classList.remove('display-none');
+        overlay.classList.remove('display-none');
         navbarMobile.classList.add('show');
         overlay.classList.add('show');
-        body.style.overflow = 'hidden';
+        body.style.overflow = "hidden";
     } else {
         navbarMobile.classList.remove('show');
         overlay.classList.remove('show');
-        body.style.overflow = 'auto';
-        setTimeout(() => {
-            navbarMobile.classList.add('display-none');
-        }, 500)
+        body.removeAttribute('style');
+        overlay.classList.add('display-none');
     }
 }
